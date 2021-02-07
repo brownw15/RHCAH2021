46058337c2dc2e9dcab5597f66665e58dc667126
govtPigeon <70912198+govtPigeon@users.noreply.github.com>
2021-02-07 23:51:58 +0500

    Changed $conn variable
    Changed $conn to $link to reflect databaseConnection.php

--- calendar/signupSubmission.php
+++ calendar/signupSubmission.php
@@ -6,7 +6,7 @@
     // This code block checks to see if user is in database before adding them to it
     //the object in $var->prepare needs to be the same object as the object created in databaseConnection.php via $var = new mysqli();
     if(isset($_POST['submit'])){
-        $stmt = $conn->prepare("SELECT account.username FROM account WHERE username=?");
+        $stmt = $link->prepare("SELECT account.username FROM account WHERE username=?");
         $stmt->bind_param('s', $_POST['username']);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -16,7 +16,6 @@
         }
         else{
             $profileExists = true;
-            $conn->close();
         }
     }
  
@@ -24,12 +23,11 @@
     if($profileExists == false){
         //id needs to be modified for each test unless working with a fresh database.
         $insert = "INSERT INTO account (id,firstname,lastname,username,email,userPassword) VALUES ('5',?,?,?,?,?)"; //prepared sql statement for efficiency and security
-	    $stmt = $conn->prepare($insert);
+	    $stmt = $link->prepare($insert);
         $stmt->bind_param("sssss", $_POST['firstname'], $_POST['lastname'], $_POST['username'], $_POST['email'], $_POST['userPassword']); //sssss for each parameter being handled as a string
 
 	    if($stmt->execute()){
             //account was successfully created
-            $conn->close();
             $_SESSION['myUsername'] = $firstname;
             echo '<script>';
             echo 'for(i=0; i<1; i++){window.location.assign("home.php")}';
@@ -38,5 +36,5 @@
     }
 
     //needs to be same as the connection variable. otherwise connection won't close successfully.
-    $conn->close();
+    $link->close();
 ?>
