ba17cea045b1901c4d2697d6bf690788e0ecfe57
govtPigeon <70912198+govtPigeon@users.noreply.github.com>
2021-02-07 23:45:17 +0500

    Made modifications so database works
    commented out line 2. not sure if _db.php is a necessary file but I don't think it is. It seems to generate conflicts.
    Deleted *DELETE* block
    changed $stmt = $db->prepare to $conn->prepare as $conn is the object created in databaseConnection.php
    further changed all following $db to $conn to match above
    the SELECT statement should always execute successfully (provided the right arguments are passed). Thus, instead of checking for successful statement execution, we need to check for the returned row amount. This change is reflected on (if !$stmt->execute()) being changed to if($result->num_rows == 0) and an additional 2 lines above the if to capture the $result variable.
    
    ********IMPORTANT************
    inserts will NOT succeed unless you update the prepared statement on line 25. the id under VALUES needs to be changed, else the database will reject the insert. (this only applies when working with an account table that already has entries).

--- calendar/signupSubmission.php
+++ calendar/signupSubmission.php
@@ -1,46 +1,42 @@
 <?php
-    require_once '_db.php';
+    //require_once '_db.php';
     session_start();
     include 'databaseConnection.php';   // we will add this file later, which holds establishing connection to database
-    
-    /*DELETE*/
-    //for now when the form is submitted it will just redirect to home.php
-    //echo '<script>';
-   // echo 'for(i=0; i<1; i++){window.location.assign("home.php")}';
-    //echo '</script>';
-
-    /*DELETE*/
 
     // This code block checks to see if user is in database before adding them to it
+    //the object in $var->prepare needs to be the same object as the object created in databaseConnection.php via $var = new mysqli();
     if(isset($_POST['submit'])){
-        $stmt = $db->prepare('SELECT username FROM account WHERE username = :username');
-        $stmt->bindParam(':username', $_POST['username']);
-
-        if (!$stmt->execute()){ // if query fails, then the account was not found
+        $stmt = $conn->prepare("SELECT account.username FROM account WHERE username=?");
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        // if query returns 0 rows, account not found.
+        if ($result->num_rows == 0){
             $profileExists = false;
         }
         else{
             $profileExists = true;
+            $conn->close();
         }
-        $stmt->close();
     }
-    
+ 
     //create profile if it doesn't already exist
     if($profileExists == false){
-        $insert = "INSERT INTO account (firstname,lastname,username,email,userPassword) Values (?,?,?,?,?)"; //prepared sql statement for efficiency and security
-	    $stmt = $db->prepare($insert);
+        //id needs to be modified for each test unless working with a fresh database.
+        $insert = "INSERT INTO account (id,firstname,lastname,username,email,userPassword) VALUES ('5',?,?,?,?,?)"; //prepared sql statement for efficiency and security
+	    $stmt = $conn->prepare($insert);
         $stmt->bind_param("sssss", $_POST['firstname'], $_POST['lastname'], $_POST['username'], $_POST['email'], $_POST['userPassword']); //sssss for each parameter being handled as a string
 
 	    if($stmt->execute()){
             //account was successfully created
-            $stmt->close();
+            $conn->close();
             $_SESSION['myUsername'] = $firstname;
             echo '<script>';
             echo 'for(i=0; i<1; i++){window.location.assign("home.php")}';
             echo '</script>';
-
 	    }
     }
 
-    $stmt->close();
+    //needs to be same as the connection variable. otherwise connection won't close successfully.
+    $conn->close();
 ?>
